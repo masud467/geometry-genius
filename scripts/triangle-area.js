2b0be15e3d54feebadf6f0dbdaaedcb0b14084de
masud4657 <masudpfrnt@gmail.com>
2024-02-13 15:43:34 +0600

    Add all my files

---
+++ scripts/triangle-area.js
@@ -0,0 +1,20 @@
+function calculateTriangleArea(){
+    const triangleBaseInput = document.getElementById('triangle-base')
+    const triangleBase = triangleBaseInput.value;
+    const base = parseFloat(triangleBase);
+    console.log(base)
+
+    const triangleHeightInput = document.getElementById('triangle-height')
+    const triangleHeight = triangleHeightInput.value;
+    const height = parseFloat(triangleHeight)
+
+    console.log(height)
+
+    const area = 0.5 * base * height;
+    console.log('triangle area:', area)
+
+
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = area
+    
+}
